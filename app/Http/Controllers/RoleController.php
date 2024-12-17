6b2edcc6e50ff31dfcfdaaf67986fa186f7168dc
Laurin49 <welter@diwe.info>
2024-12-17 09:11:08 +0100

    dev09 Assign Permissions to Roles o.k.

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreRoleRequest;
+use App\Http\Resources\PermissionResource;
 use App\Http\Resources\RoleResource;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
@@ -26,7 +29,9 @@ class RoleController extends Controller
    */
   public function create(): Response
   {
-    return Inertia::render('Admin/Roles/Create');
+    return Inertia::render('Admin/Roles/Create', [
+      'permissions' => PermissionResource::collection(Permission::all())
+    ]);
   }
 
   /**
@@ -34,7 +39,10 @@ class RoleController extends Controller
    */
   public function store(StoreRoleRequest $request)
   {
-    Role::create($request->validated());
+    $role = Role::create(['name' => $request->name]);
+    if ($request->has('permissions')) {
+      $role->syncPermissions($request->input('permissions.*.name'));
+    }
     return to_route('roles.index');
   }
 
@@ -51,23 +59,24 @@ class RoleController extends Controller
    */
   public function edit(Role $role)
   {
-    // $role = Role::findById($id);
-    // $role->load('permissions');
+    $role->load('permissions');
 
     return Inertia::render('Admin/Roles/Edit', [
       'role' => new RoleResource($role),
-      // 'permissions' => PermissionResource::collection(Permission::all())
+      'permissions' => PermissionResource::collection(Permission::all())
     ]);
   }
 
   /**
    * Update the specified resource in storage.
    */
-  public function update(StoreRoleRequest $request, Role $role)
+  public function update(StoreRoleRequest $request, Role $role): RedirectResponse
   {
-    $role->update($request->validated());
-    // $role->syncPermissions($request->input('permissions.*.name'));
-    return to_route('roles.index');
+    $role->update([
+      'name' => $request->name
+    ]);
+    $role->syncPermissions($request->input('permissions.*.name'));
+    return back();
   }
 
   /**
--- resources/js/Pages/Admin/Roles/Edit.vue
+++ resources/js/Pages/Admin/Roles/Edit.vue
@@ -5,60 +5,128 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import InputError from "@/Components/InputError.vue";
 import TextInput from "@/Components/TextInput.vue";
-import { onMounted } from "vue";
+import VueMultiselect from "vue-multiselect";
+import Table from "@/Components/Table.vue";
+import TableRow from "@/Components/TableRow.vue";
+import TableDataCell from "@/Components/TableDataCell.vue";
+import TableHeaderCell from "@/Components/TableHeaderCell.vue";
+
+import { onMounted, watch } from "vue";
 
 const props = defineProps({
-  role: {
-    type: Object,
-    required: true,
-  },
+    role: {
+        type: Object,
+        required: true,
+    },
+    permissions: Array,
 });
 const form = useForm({
-  name: props.role.name,
+    name: props.role.name,
+    permissions: [],
 });
 
 onMounted(() => {
+  form.permissions = props.role?.permissions
 });
-
+watch(
+  () => props.role,
+  () => (form.permissions = props.role?.permissions)
+);
 </script>
 
 <template>
-  <Head title="Update role" />
+    <Head title="Update role" />
 
-  <AdminLayout>
-    <div class="py-4 mx-auto max-w-7xl">
-      <div class="flex justify-between">
-        <Link
-          :href="route('roles.index')"
-          class="px-3 py-2 font-semibold text-white bg-indigo-500 rounded hover:bg-indigo-700"
-          >Back
-        </Link>
-      </div>
-      <div class="max-w-6xl p-6 mx-auto mt-6 rounded-lg shadow-lg bg-slate-100">
-        <h1 class="text-2xl font-semibold text-indigo-700">Update role</h1>
-        <form @submit.prevent="form.put(route('roles.update', role.id))">
-          <div class="mt-4">
-            <InputLabel for="name" value="Name" />
-            <TextInput
-              id="name"
-              type="text"
-              class="block w-full mt-1"
-              v-model="form.name"
-              autofocus
-            />
-            <InputError class="mt-2" :message="form.errors.name" />
-          </div>
-          
-          <div class="flex items-center mt-4">
-            <PrimaryButton
-              :class="{ 'opacity-25': form.processing }"
-              :disabled="form.processing"
+    <AdminLayout>
+        <div class="py-4 mx-auto max-w-7xl">
+            <div class="flex justify-between">
+                <Link
+                    :href="route('roles.index')"
+                    class="px-3 py-2 font-semibold text-white bg-indigo-500 rounded hover:bg-indigo-700"
+                    >Back
+                </Link>
+            </div>
+            <div
+                class="max-w-6xl p-6 mx-auto mt-6 rounded-lg shadow-lg bg-slate-100"
             >
-              Update
-            </PrimaryButton>
-          </div>
-        </form>
-      </div>
-    </div>
-  </AdminLayout>
+                <h1 class="text-2xl font-semibold text-indigo-700">
+                    Update role
+                </h1>
+                <form
+                    @submit.prevent="form.put(route('roles.update', role.id))"
+                >
+                    <div class="mt-4">
+                        <InputLabel for="name" value="Name" />
+                        <TextInput
+                            id="name"
+                            type="text"
+                            class="block w-full mt-1"
+                            v-model="form.name"
+                            autofocus
+                        />
+                        <InputError class="mt-2" :message="form.errors.name" />
+                    </div>
+                    <div class="mt-4">
+                        <InputLabel for="permissions" value="Permissions" />
+                        <VueMultiselect
+                            v-model="form.permissions"
+                            :options="permissions"
+                            :multiple="true"
+                            :close-on-select="true"
+                            placeholder="Pick some"
+                            label="name"
+                            track-by="id"
+                        />
+                    </div>
+                    <div class="flex items-center mt-4">
+                        <PrimaryButton
+                            :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing"
+                        >
+                            Update
+                        </PrimaryButton>
+                    </div>
+                </form>
+            </div>
+            <div
+                class="max-w-6xl p-6 mx-auto mt-6 rounded-lg shadow-lg bg-slate-100"
+            >
+                <h1 class="text-2xl font-semibold text-indigo-700">
+                    Permissions
+                </h1>
+                <div class="bg-white">
+                    <Table class="mt-4">
+                        <template #header>
+                            <TableRow>
+                                <TableHeaderCell>ID</TableHeaderCell>
+                                <TableHeaderCell>Name</TableHeaderCell>
+                                <TableHeaderCell>Action</TableHeaderCell>
+                            </TableRow>
+                        </template>
+                        <template #default>
+                            <TableRow
+                                v-for="rolePermission in role.permissions"
+                                :key="rolePermission.id"
+                                class="border-b"
+                            >
+                                <TableDataCell>{{rolePermission.id}}</TableDataCell>
+                                <TableDataCell>{{rolePermission.name}}</TableDataCell>
+                                <TableDataCell class="space-x-4">
+                                    <Link
+                                        :href="route('roles.permissions.destroy', 
+                                          [role.id, rolePermission.id,])"
+                                        method="DELETE"
+                                        as="button"
+                                        class="text-red-400 hover:text-red-600">
+                                        Revoke
+                                    </Link>
+                                </TableDataCell>
+                            </TableRow>
+                        </template>
+                    </Table>
+                </div>
+            </div>
+        </div>
+    </AdminLayout>
 </template>
+<style src="vue-multiselect/dist/vue-multiselect.css"></style>
